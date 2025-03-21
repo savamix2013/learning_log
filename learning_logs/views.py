c8565e8cefd90dc97cf5b5011eac13a6fd6c3d9f
Vsevolod Parashchyn <sevaparashchyn@gmail.com>
2025-03-21 20:30:30 +0200

    змінена приватність

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -11,7 +12,7 @@ def index(request):
 @login_required
 def topics(request):
     """Виводить всі теми"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
@@ -19,6 +20,9 @@ def topics(request):
 def topic(request, topic_id):
     """Виводить всі записи для однієї теми"""
     topic = Topic.objects.get(id=topic_id)
+    # Перевірка того, чи тема належить поточному користувачеві
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -33,7 +37,9 @@ def new_topic(request):
         # Відправлений POST-запит; обробити дані.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     # Показати порожню або недійсну форму
@@ -66,6 +72,8 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
